6ad1f77484f46f40c7bb1c18d6a55efef0e9861a
Nick <Nick@thegrid-2>
2017-01-19 20:07:00 -0500

    Added robot server classes

---
+++ Team3044-2017/src/org/usfirst/frc/team3044/Reference/RobotHttpServerDispatchResponse.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team3044.Reference;
+
+import org.nanohttpd.protocols.http.response.IStatus;
+
+public class RobotHttpServerDispatchResponse {
+	public IStatus Status;
+	public Object ResponseData; 
+	public String StatusMessage; 
+}
