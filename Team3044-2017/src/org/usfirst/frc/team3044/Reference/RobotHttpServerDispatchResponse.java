8edb4f90f2d3a5dfbe84f6407a4f1746695130c4
Nick <Nick@thegrid-2>
2017-01-19 19:45:36 -0500

    Fixing build

--- Team3044-2017/src/org/usfirst/frc/team3044/Reference/RobotHttpServerDispatchResponse.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team3044.Reference;
-
-import org.nanohttpd.protocols.http.response.IStatus;
-
-public class RobotHttpServerDispatchResponse {
-	public IStatus Status;
-	public Object ResponseData; 
-	public String StatusMessage; 
-}
