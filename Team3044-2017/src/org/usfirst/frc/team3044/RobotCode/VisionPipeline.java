f71e51722278fd89b3661096ae01c2b14e6e1489
Ryan Keihm <ryan.keihm@gmail.com>
2017-01-30 18:56:09 -0500

    Added A Vision Pipeline Java Class

---
+++ Team3044-2017/src/org/usfirst/frc/team3044/RobotCode/VisionPipeline.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team3044.RobotCode;
+
+import org.usfirst.frc.team3044.Reference.*;
+import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class visionPipelinePipeline {
+
+	public void visionPipelineAutoInit() {
+	}
+
+	public void visionPipelineAutoPeriodic() {
+	}
+
+	public void visionPipelineInit() {
+	}
+
+	public void visionPipelineTeleopPeriodic() {
+	}
+
+	public void testInit() {
+	}
+
+	public void testPeriodic() {
+	}
+}
