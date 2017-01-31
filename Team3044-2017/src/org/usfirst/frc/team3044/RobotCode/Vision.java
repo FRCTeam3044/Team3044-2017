dbb17967c0b713b78dbd6c653a10397e96bb26f1
Colin <colindude@nycap.rr.com>
2017-01-30 19:37:41 -0500

    GripPipeline and Vision code added

--- Team3044-2017/src/org/usfirst/frc/team3044/RobotCode/Vision.java
+++ Team3044-2017/src/org/usfirst/frc/team3044/RobotCode/Vision.java
@@ -1,28 +1,47 @@
 package org.usfirst.frc.team3044.RobotCode;
 
-import org.usfirst.frc.team3044.Reference.*;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
 import com.ctre.CANTalon;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.vision.VisionRunner;
+import edu.wpi.first.wpilibj.vision.VisionThread;
 
-public class Vision {
+public class Vision extends IterativeRobot {
+	private static final int IMG_WIDTH = 320;
 
-	public void visionAutoInit() {
+	private static final int IMG_HEIGHT = 240;
+	private VisionThread visionThread;
+	private double centerX = 0.0;
+	private RobotDrive drive;
+	private final Object imgLock = new Object();
+
+	@Override
+	public void robotInit() {
+		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();  //Info on this at https://www.chiefdelphi.com/forums/showpost.php?p=1425235&postcount=6
+		camera.setResolution(IMG_WIDTH, IMG_HEIGHT);							//Above, put camera name inside () of startAutomaticCapture() in quotes
+		visionThread = new VisionThread(camera, new GripPipeline(), pipeline -> {
+			if (!pipeline.filterContoursOutput().isEmpty()) {
+				Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+				synchronized (imgLock) {
+					centerX = r.x + (r.width / 2);
+				}
+			}
+		});
+		visionThread.start();
+		drive = new RobotDrive(1, 2);
 	}
 
-	public void visionAutoPeriodic() {
-	}
-
-	public void visionInit() {
-	}
-
-	public void visionTeleopPeriodic() {
-	}
-
-	public void testInit() {
-	}
-
-	public void testPeriodic() {
+	@Override
+	public void autonomousPeriodic() {
+		double centerX;
+		synchronized (imgLock) {
+			centerX = this.centerX;
+		}
+		double turn = centerX - (IMG_WIDTH / 2);
+		drive.arcadeDrive(-0.6, turn * 0.005);
 	}
 }
