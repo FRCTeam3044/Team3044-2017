269e1552860f79d0c8531138ced6493ef340a9ec
Colin <colindude@nycap.rr.com>
2017-03-10 18:09:56 -0500

    Variable names updated.

--- Team3044-2017/src/org/usfirst/frc/team3044/RobotCode/VisionProcessingThread.java
+++ Team3044-2017/src/org/usfirst/frc/team3044/RobotCode/VisionProcessingThread.java
@@ -16,24 +16,29 @@ public class VisionProcessingThread extends Thread {
 	public boolean Run;
 	// static AxisCamera FrontCamera;
 	static GripPipeline pipeline = new GripPipeline();
+
+	// All the variables needed to process things in vision
 	public int rect1_x;
 	public int rect2_x;
 	public int rect1_y;
 	public int rect2_y;
+	public int rect1_width;
+	public int rect2_width;
 	public double rect1_area;
 	public double rect2_area;
+	double area_difference;
+	public double rect1_centerx;
+	public double rect1_centery;
+	public double rect2_centerx;
+	public double rect2_centery;
+	double center_of_board;
 	public int n_rectangles;
-	public double center_1_x;
-	public double center_1_y;
-
-	public double center_2_x;
-	public double center_2_y;
 
 	public VisionProcessingThread() {
 	}
 
 	public void run() {
-		Run=true;
+		Run = true;
 		while (Run) {
 
 			SimpleDateFormat f = new SimpleDateFormat("HH:mm:ss.SSS");
@@ -43,7 +48,7 @@ public class VisionProcessingThread extends Thread {
 
 			try {
 				// CameraServer.getInstance().getVideo().grabFrame(image);
-				CameraServer.getInstance().getVideo(Vision.FrontCamera).grabFrame(image);
+				CameraServer.getInstance().getVideo(Vision.FrontCamera).grabFrameNoTimeout(image);
 			} catch (Exception e) {
 				// e.printStackTrace();
 			}
@@ -65,26 +70,48 @@ public class VisionProcessingThread extends Thread {
 
 					Rect[] rectangles = new Rect[pipeline.filterContoursOutput().size()];
 
+					Rect leftRectangle;
+					Rect rightRectangle;
+
 					for (int i = 0; i < pipeline.filterContoursOutput().size(); i++) {
 						rectangles[i] = Imgproc.boundingRect(pipeline.filterContoursOutput().get(i));
 
 					}
 
-					rect1_x = rectangles[0].x;
-					rect1_y = rectangles[0].y;
-					rect1_area = rectangles[0].area();
-					rect2_x = rectangles[1].x;
-					rect2_y = rectangles[1].y;
-					rect2_area = rectangles[1].area();
+					// find rect with larger x and set rightRectangle = it
+					// other goes into leftRectangle
+					if (rectangles[0].x > rectangles[1].x) {
+						rightRectangle = rectangles[0];
+						leftRectangle = rectangles[1];
+					} else {
+						rightRectangle = rectangles[1];
+						leftRectangle = rectangles[0];
+					}
+
+					// Sets the variables to what was found by the array of rectangles
+					// Rect1 will ALWAYS be the left rectangle, Rect2 will ALWAYS be right rectangle
+					rect1_x = leftRectangle.x;
+					rect1_y = leftRectangle.y;
+					rect2_x = rightRectangle.x;
+					rect2_y = rightRectangle.y;
+					rect1_area = leftRectangle.area();
+					rect2_area = rightRectangle.area();
 					n_rectangles = rectangles.length;
+					rect1_width = leftRectangle.width;
+					rect2_width = rightRectangle.width;
+					rect1_centerx = rect1_x + (leftRectangle.width / 2);
+					rect1_centery = rect1_y + (leftRectangle.height / 2);
+					rect2_centerx = rect2_x + (rightRectangle.width / 2);
+					rect2_centery = rect2_y + (rightRectangle.height / 2);
+					area_difference = (rect1_area - rect2_area);
 
-					center_1_x = rect1_x + (rectangles[0].width / 2);
-					center_1_y = rect1_y + (rectangles[0].height / 2);
-
-					center_2_x = rect2_x + (rectangles[1].width / 2);
-					center_2_y = rect2_y + (rectangles[1].height / 2);
-
-
+					// Makes sure that the rectangle on the left is ALWAYS rectangle 1
+					boolean r1 = (rect1_x < rect2_x);
+					if (r1) {
+						center_of_board = (rect1_x + rect2_x + rect2_width) / 2;
+					} else {
+						center_of_board = (rect2_x + rect1_x + rect1_width) / 2;
+					}
 				}
 			}
 		}
